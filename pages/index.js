59bb2e38b44d5a014cefa53d4f4f17d804d85534
Restyled.io <commits@restyled.io>
2021-06-15 01:33:07 +0000

    Restyled by clang-format

--- pages/index.js
+++ pages/index.js
@@ -1,90 +1,87 @@
-import Head from "next/head";
+import {CheckCircleIcon, CopyIcon, MoonIcon, SunIcon} from "@chakra-ui/icons";
 import {
+  Button,
   Flex,
   Heading,
   Input,
-  Button,
+  useClipboard,
   useColorMode,
   useColorModeValue,
   useToast,
-  useClipboard,
 } from "@chakra-ui/react";
-import { MoonIcon, SunIcon, CopyIcon, CheckCircleIcon } from "@chakra-ui/icons";
-import { useRef, useEffect } from "react";
+import Head from "next/head";
+import {useEffect, useRef} from "react";
+
 export default function Home() {
-  const { toggleColorMode } = useColorMode();
+  const {toggleColorMode} = useColorMode();
   const toast = useToast();
   const linkInputRef = useRef(null);
   const shortRef = useRef(0);
-  const { hasCopied, onCopy } = useClipboard(shortRef.current.value);
+  const {hasCopied, onCopy} = useClipboard(shortRef.current.value);
   const handleForm = async () => {
-    if (linkInputRef.current.value == "") return;
+    if (linkInputRef.current.value == "")
+      return;
     const res = await fetch("/api/create", {
-      body: JSON.stringify({
-        link: linkInputRef.current.value,
+      body : JSON.stringify({
+        link : linkInputRef.current.value,
       }),
-      headers: {
-        "Content-Type": "application/json",
+      headers : {
+        "Content-Type" : "application/json",
       },
-      method: "POST",
+      method : "POST",
     });
 
-    const { error, short_link } = await res.json();
+    const {error, short_link} = await res.json();
     if (error) {
       toast({
-        title: "Error!",
-        description: error,
-        status: "error",
-        duration: 5000,
-        isClosable: true,
+        title : "Error!",
+        description : error,
+        status : "error",
+        duration : 5000,
+        isClosable : true,
       });
       return;
     } else if (short_link)
       shortRef.current.value = `${process.env.PROJECT_URL}/${short_link}`;
     linkInputRef.current.value = "";
     toast({
-      title: "Link created.",
-      description: `Your link is ready at ${process.env.PROJECT_URL}/${short_link}`,
-      status: "success",
-      duration: 5000,
-      isClosable: true,
+      title : "Link created.",
+      description :
+          `Your link is ready at ${process.env.PROJECT_URL}/${short_link}`,
+      status : "success",
+      duration : 5000,
+      isClosable : true,
     });
   };
-  
+
   useEffect(() => {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    if (window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches) {
       toggleColorMode();
     }
   }, []);
-  
+
   return (
     <Flex height="100vh" alignItems="center" justifyContent="center">
       <Flex
-        direction="column"
-        background={useColorModeValue("gray.400", "gray.700")}
-        p={10}
-        rounded={6}
-      >
-        <Button colorScheme="teal" onClick={toggleColorMode} ml={48} mb={-10}>
-          {useColorModeValue(<MoonIcon />, <SunIcon />)}
-        </Button>
+  direction = "column"
+  background = {useColorModeValue("gray.400", "gray.700")} p = {10} rounded =
+      {6} >
+      <Button colorScheme =
+           "teal" onClick = {toggleColorMode} ml = {48} mb = {-10}>{
+          useColorModeValue(<MoonIcon />, <SunIcon />)}<
+          /Button>
         <Heading mb={6} color="blue.500">
           Add a link!
-        </Heading>
-        <Input
-          ref={linkInputRef}
-          variant="filled"
-          mb={3}
-          type="url"
-          placeholder="Long URL (max 500 chars)"
-          isRequired
-        />
-        <Input
-          ref={shortRef}
-          variant="filled"
-          mb={3}
-          type="url"
-          placeholder="shortened link"
+        </Heading><
+      Input
+  ref = {linkInputRef} variant = "filled"
+  mb = {3} type = "url"
+  placeholder = "Long URL (max 500 chars)"
+  isRequired / > < Input
+  ref = {shortRef} variant = "filled"
+  mb = {3} type = "url"
+  placeholder = "shortened link"
           isReadOnly
         />
         <Button colorScheme="blue" onClick={onCopy} mb={10} ml={64}>
